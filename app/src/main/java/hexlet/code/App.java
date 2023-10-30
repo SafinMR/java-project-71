cd727bb0d3c606dc56868ffcd31ccbe6263f68de
SafinMR <safinmr229@gmail.com>
2023-10-31 01:06:19 +0300

    add picocli help options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
+    boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+    boolean versionInfoRequested;
+
+    @Override
+    public Integer call() throws Exception {
+
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        App app = CommandLine.populateCommand(new App(), args);
+        if (app.usageHelpRequested) {
+            CommandLine.usage(new App(), System.out);
+        } else if (app.versionInfoRequested) {
+            System.out.println("1.0");
+        }
     }
 }
