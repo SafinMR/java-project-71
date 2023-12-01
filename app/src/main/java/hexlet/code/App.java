973b4066687d236c4ab4f63ffde57c82d0f8fe72
SafinMR <safinmr229@gmail.com>
2023-12-01 03:32:11 +0300

    minor fixes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,18 +3,29 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
     boolean usageHelpRequested;
 
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
     boolean versionInfoRequested;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private File filepath2;
 
     @Override
     public Integer call() throws Exception {
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,49 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Differ {
-    public static String generate() {
-        return "Hello,world!";
+    public static String generate(String filepath1, String filepath2) throws Exception {
+
+        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
+
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + path1 + "' does not exist");
+        }
+
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + path2 + "' does not exist");
+        }
+
+        String json1 = Files.readString(path1);
+        String json2 = Files.readString(path2);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String, Object> map1 = mapper.readValue(json1, new TypeReference<Map<String,Object>>(){});
+        Map<String, Object> map2 = mapper.readValue(json2, new TypeReference<Map<String,Object>>(){});
+
+        Map<String, Object> mapAll = new TreeMap<>();
+        mapAll.putAll(map1);
+        mapAll.putAll(map2);
+
+        List<String> listDiff = new ArrayList<>();
+
+        for (Map.Entry<String, Object> entry : mapAll.entrySet()) {
+            String key = entry.getKey();
+
+
+        }
+
     }
 }
